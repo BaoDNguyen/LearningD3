75d07d276b2edac341410dc21bccf1b6ac5af826
BaoDNguyen <Bao.D.Nguyen@ttu.edu>
2020-03-15 22:02:46 -0500

    abc

--- LearningD3/js/changeFormatData.js
+++ LearningD3/js/changeFormatData.js
@@ -10,5 +10,12 @@ Promise.all([
     for (let row = 0; row < file[2].length; row++) {
         sectorArr[row] = [file[2][row].code,file[2][row].name];
     }
-    
+    for (let row = 0; row < file[0].length; row++) {
+        let instance = file[0][row]['Series ID'].substr(3,2);
+        let variable = file[0][row]['Series ID'].substr(11,4);
+        let instanceIndex = stateArr.findIndex(element=>element[0]===instance);
+        let variableIndex = sectorArr.findIndex(element=>element[0]===variable);
+        let seriesID = instanceIndex.toString()+'_'+variableIndex.toString();
+        
+    }
 });
