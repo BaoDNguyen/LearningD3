f25a9e5d5b095bdfe4c1e3142e4fa5f477b246ef
BaoDNguyen <Bao.D.Nguyen@ttu.edu>
2020-03-16 14:50:17 -0500

    abc

--- LearningD3/js/changeFormatData.js
+++ LearningD3/js/changeFormatData.js
@@ -1,21 +1,60 @@
 Promise.all([
-    d3.csv('data/#establish.txt'),
-    d3.csv('data/stateCode.txt'),
-    d3.csv('data/sectors_code.txt'),
+    d3.csv('data/house_price.txt'),
+    d3.csv('data/us_population_1969-2017.csv'),
+    d3.tsv('data/state_code.txt'),
 ]).then(function(file){
-    let stateArr = [], sectorArr = [];
-    for (let row = 0; row < file[1].length; row++) {
-        stateArr[row] = [file[1][row].code,file[1][row].name];
-    }
+    let stateArr = [], variableArr = [];
     for (let row = 0; row < file[2].length; row++) {
-        sectorArr[row] = [file[2][row].code,file[2][row].name];
+        stateArr[row] = [file[2][row].code,file[2][row].name,row];
     }
+    variableArr[0] = [0,'house price'];
+    variableArr[1] = [1,'population'];
+    Bao.myData.data = {housePrice: [], population: []};
+    Bao.myData.data.housePrice[0] = [];
+    Bao.myData.data.housePrice[0].push('Series ID');
+    for (let year = 1991; year < 2018; year++) {
+        Bao.myData.data.housePrice[0].push(year.toString());
+    }
+    let correctRow = 0;
+    let instance = -1;
+    let annualPrice;
     for (let row = 0; row < file[0].length; row++) {
-        let instance = file[0][row]['Series ID'].substr(3,2);
-        let variable = file[0][row]['Series ID'].substr(11,4);
-        let instanceIndex = stateArr.findIndex(element=>element[0]===instance);
-        let variableIndex = sectorArr.findIndex(element=>element[0]===variable);
-        let seriesID = instanceIndex.toString()+'_'+variableIndex.toString();
-        
+        if (+file[0][row]['yr'] < 2018) {
+            let lastInstance = instance;
+            instance = stateArr.findIndex(element=>element[0]===file[0][row].state);
+            let variable = 0;
+            let seriesID = instance.toString()+'_'+variable.toString();
+            if (row%4===0) annualPrice = +file[0][row]['index_sa'];
+            else if (row%4===3) {
+                annualPrice += +file[0][row]['index_sa'];
+                annualPrice /= 4;
+            } else annualPrice += +file[0][row]['index_sa'];
+            if (instance!==lastInstance) {
+                correctRow += 1;
+                Bao.myData.data.housePrice[correctRow] = [];
+                Bao.myData.data.housePrice[correctRow].push(seriesID);
+            } else {
+                if (row%4===3) Bao.myData.data.housePrice[correctRow].push(annualPrice);
+            }
+        }
+    }
+    Bao.myData.data.population[0] = [];
+    Bao.myData.data.population[0].push('Series ID');
+    for (let year = 1991; year < 2018; year++) {
+        Bao.myData.data.population[0].push(year.toString());
+    }
+    correctRow = 0;
+    for (let row = 0; row < file[1].length; row++) {
+        let instance = stateArr.findIndex(element=>element[0]===file[1][row]['State']);
+        let variable = 1;
+        let seriesID = instance.toString()+'_'+variable.toString();
+        if (+file[1][row]['Year'] > 1990) {
+                if (+file[1][row]['Year'] === 1991) {
+                    correctRow += 1;
+                    Bao.myData.data.population[correctRow] = [];
+                    Bao.myData.data.population[correctRow].push(seriesID);
+                    Bao.myData.data.population[correctRow].push(file[1][row]['Population']);
+                } else Bao.myData.data.population[correctRow].push(file[1][row]['Population']);
+        }
     }
 });
